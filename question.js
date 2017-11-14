5efaf231f5595442bae64cac40a09413ef07ffdb
Chris Pang <chris_pang429@outlook.com>
2017-11-14 02:52:16 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,7 +1,14 @@
 'use strict';
 
 class Question {
+  constructor() {
+    this.save()
+  }
 
+  save() {
+    this.constructor._All.push(this)
+  }
+  
   //The static keyword defines the Class Function
   static All() {
     return this._All;
