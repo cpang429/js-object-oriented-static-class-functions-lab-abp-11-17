8dd97afd5a2024df7fa176e7dddacec1e7df4696
Chris Pang <chris_pang429@outlook.com>
2017-11-14 02:48:01 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,25 +1,5 @@
 'use strict';
 
 class Question {
-  
-/*  constructor(ID) {
-    this.ID = ID
-    this.save()
-  } */
-
-  /*save() {
-    this.constructor._All.push(this)
-  }
-/*
-  Static All() {
-    return this.All;
-  }
-
-  static FindByID(ID) {
-    return this.All().filter(function(user) {
-      return user.ID === ID;
-    })
-  }
-*/
 
 }
