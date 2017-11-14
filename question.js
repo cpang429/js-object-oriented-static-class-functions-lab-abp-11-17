f13454f0f471191022efc6d33cfec97960e01110
Chris Pang <chris_pang429@outlook.com>
2017-11-14 03:13:04 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -16,12 +16,12 @@ class Question {
   }
 
   //reference the ID of the question
-  static Find(id) {
+  /*static Find(id) {
     // id 1
     // [Question1, Question2, Question3]
     // index 0 id-1
     return this.All()[id-1]
-  }
+  }*/
 }
 
 }
