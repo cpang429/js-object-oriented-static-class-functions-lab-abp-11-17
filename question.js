cd4a0239eca30c8d26741042d289975d3e06bac6
Chris Pang <chris_pang429@outlook.com>
2017-11-14 03:23:25 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Question {
+  constructor(content) {
+    this.content = content;
+    this.save()
+  }
+
+  save() {
+    this.constructor._All.push(this)
+  }
+
+  //The static keyword defines the Class Function
+  static All() {
+    return this._All;
+  }
+
+  static Find(id) {
+    return this.All()[id-1]
+  }
+}
+
+Question._All = [];
